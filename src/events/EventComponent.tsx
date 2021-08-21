5cb7c8f446214d1bff5b47a9f072bb7907a92f08
Shailesh <shailesh.u.mishra@gmail.com>
2021-08-22 04:49:09 +0530

    017 016-rts03 Types Around Events and Refs

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -3,9 +3,16 @@ const EventComponent: React.FC = () => {
     console.log(event);
   };
 
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log(event);
+  };
+
   return (
     <div>
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me!
+      </div>
     </div>
   );
 };
